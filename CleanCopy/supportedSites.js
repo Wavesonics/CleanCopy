fe468fb673ac1170bd1ba3074c61e622a4333a9c
Adam Brown <adamwbrown@gmail.com>
2013-01-16 00:24:32 -0800

    0.9 Release Part 2
    Added Googe Play pattern for App pages

--- CleanCopy/supportedSites.js
+++ CleanCopy/supportedSites.js
@@ -5,7 +5,7 @@
 		[
 			{
 				captureGroups : 1,
-				capturePattern : /^(?:http|https):\/\/(?:www.)?youtube.com\/watch\?.*v=([a-zA-Z0-9_-]+).*$/i,
+				capturePattern : /^(?:http|https):\/\/(?:www.)?youtube.com\/watch\?(?:.*&)*v=([a-zA-Z0-9_-]+).*$/i,
 				urlTemplate : "http://youtube.com/watch?v=URL_ID_0_HERE&hd=1",
 				isDefault : true
 			}
@@ -51,18 +51,31 @@
 		isDefault : true
 	},
 	{
+		domain : "play.google.com",
+		pages :
+		[
+			{
+				captureGroups : 1,
+				capturePattern : /^(?:http|https):\/\/play.google.com\/store\/apps\/details\?(?:.*&)*id=((?:[a-zA-Z0-9_.])+).*$/i,
+				urlTemplate : "https://play.google.com/store/apps/details?id=URL_ID_0_HERE",
+				isDefault : true
+			}
+		],
+		isDefault : true
+	},
+	{
 		domain : "maps.google.com",
 		pages :
 		[
 			{
 				captureGroups : 3,
-				capturePattern : /^(?:http|https):\/\/maps.google.com\/maps\?.*(?:q=([',\+a-zA-Z0-9_-]+)).*(?:(?:&|\?)ll=(-?\d+\.\d+,-?\d+\.\d+)).*(?:&z=(\d+)).*$/i,
+				capturePattern : /^(?:http|https):\/\/maps.google.com\/maps\?(?:.*&)*(?:q=([',\+a-zA-Z0-9_-]+)).*(?:(?:&|\?)ll=(-?\d+\.\d+,-?\d+\.\d+))(?:.*&)*(?:&z=(\d+)).*$/i,
 				urlTemplate : "https://maps.google.com/maps?q=URL_ID_0_HERE&ll=URL_ID_1_HERE&z=URL_ID_2_HERE",
 				isDefault : true
 			},
 			{
 				captureGroups : 2,
-				capturePattern : /^(?:http|https):\/\/maps.google.com\/maps\?.*(?:q=([',\+a-zA-Z0-9_-]+)).*(?:&z=(\d+)).*$/i,
+				capturePattern : /^(?:http|https):\/\/maps.google.com\/maps\?(?:.*&)*(?:q=([',\+a-zA-Z0-9_-]+))(?:.*&)*(?:z=(\d+)).*$/i,
 				urlTemplate : "https://maps.google.com/maps?q=URL_ID_0_HERE&z=URL_ID_1_HERE",
 				isDefault : true
 			}
